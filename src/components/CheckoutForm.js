230067e4c66a6ff6b69af8d2d1b119334d81e1ab
Daniel Hefner <danielhefner@daniels-mbp.gateway.ht.net>
2021-07-09 09:19:19 -1000

    tests written and passing (Im a little unsure about the last test but it passed)

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -33,7 +33,7 @@ const CheckoutForm = (props) => {
     <>
       <form onSubmit={handleSubmit}>
         <h2>Checkout Form</h2>
-        <label>
+        <label htmlFor='firstName'>
           First Name:
           <input
             name="firstName"
@@ -41,7 +41,7 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor='lastName'>
           Last Name:
           <input
             name="lastName"
@@ -49,7 +49,7 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor='address'>
           Address:
           <input
             name="address"
@@ -57,15 +57,15 @@ const CheckoutForm = (props) => {
             onChange={handleChanges}
           />
         </label>
-        <label>
+        <label htmlFor='city'>
           City:
           <input name="city" value={values.city} onChange={handleChanges} />
         </label>
-        <label>
+        <label htmlFor='state'>
           State:
           <input name="state" value={values.state} onChange={handleChanges} />
         </label>
-        <label>
+        <label htmlFor='zip'>
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
